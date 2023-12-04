b30f084894e452ca5bf1f0866f352f4ba505e049
JooYoung2274 <jooooo2274@gmail.com>
2023-12-04 17:47:14 +0900

    test

--- scripts/pullDocker.sh
+++ scripts/pullDocker.sh
@@ -2,4 +2,4 @@
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 853456226543.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # pull docker image
-docker pull 352200611659.dkr.ecr.ap-northeast-2.amazonaws.com/test-joo:lastest
+docker pull 853456226543.dkr.ecr.ap-northeast-2.amazonaws.com/test-joo:lastest
